79625d6863a9715f7613a6e2d620dcc075d6f480
Brian Severa <bsevera@new.rr.com>
2022-11-30 19:20:07 -0600

    changed logic to hide quit button for webGL build

--- Assets/Scripts/Main_Menu/MainMenu.cs
+++ Assets/Scripts/Main_Menu/MainMenu.cs
@@ -3,6 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _quitButton;
+
+    public void Start()
+    {
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            _quitButton.SetActive(false);
+        }
+
+    }
     public void LoadGame()
     {
         SceneManager.LoadScene("Game"); //scene 2 = game
